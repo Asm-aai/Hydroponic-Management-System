38be8957302bbd68b3b18838863113f1612968dc
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2025-10-25 17:20:54 +0900

    AddDesign

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,11 +1,6 @@
 class MeasurementsController < ApplicationController
   before_action :set_plant
 
-  def new
-    @masurement = Masurement.new
-    @plant = Plant.find(params[:plant_id])
-  end
-
   def create
     # @masurement = Masurement.new(measurement_params)
     @measurement = @plant.measurements.build(measurement_params)
@@ -16,19 +11,20 @@ class MeasurementsController < ApplicationController
       render "plants/show", status: :unprocessable_entity
     end
   end
-  
-  def index
-    @masurements = Masurement.all
-  end
-
-  def show
-    @masurement = Masurement.find(params[:id])
-    @plant = Plant.find(params[:plant_id])
-  end
 
   def edit
-    @masurement = Masurement.find(params[:id])
+    @measurement = Measurement.find(params[:id])
     @plant = Plant.find(params[:plant_id])
+  end
+
+  def update
+    @measurement = Measurement.find(params[:id])
+    @plant = Plant.find(params[:plant_id])
+    if @measurement.update(measurement_params)
+      redirect_to plant_path(@plant), notice: "計測値を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,7 +1,6 @@
 class Measurement < ApplicationRecord
   belongs_to :plant
 
-  # enum measurement_type: { temperature: 0, humidity: 1, ec: 2, ph: 3 }
   enum :measurement_type, [ :temperature, :humidity, :ec, :ph ]
   def measurement_type_i18n
     I18n.t("enums.measurement.measurement_type.#{measurement_type}")
