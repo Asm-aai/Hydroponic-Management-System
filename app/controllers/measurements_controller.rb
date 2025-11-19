729607e6f873139866aec7ade9558fa4f89b0037
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2025-11-19 19:43:13 +0900

    measurement

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -2,8 +2,7 @@ class MeasurementsController < ApplicationController
   before_action :set_plant, except: [:all]
 
   def create
-    # @masurement = Masurement.new(measurement_params)
-    @measurement = @plant.measurements.build(measurement_params)
+    @measurement = @plant.measurements.new(measurement_params)
     if @measurement.save
       redirect_to plant_path(@plant), notice: "計測値を登録しました。"
     else
@@ -14,12 +13,12 @@ class MeasurementsController < ApplicationController
 
   def edit
     @measurement = Measurement.find(params[:id])
-    @plant = Plant.find(params[:plant_id])
+    # @plant = Plant.find(params[:plant_id])
   end
 
   def update
     @measurement = Measurement.find(params[:id])
-    @plant = Plant.find(params[:plant_id])
+    # @plant = Plant.find(params[:plant_id])
     if @measurement.update(measurement_params)
       redirect_to plant_path(@plant), notice: "計測値を更新しました。"
     else
@@ -40,11 +39,29 @@ class MeasurementsController < ApplicationController
 
   private
 
+  def type_to_integer(type)
+    case type
+    when "temperature" then 0
+    when "humidity" then 1
+    when "ec" then 2
+    when "ph" then 3
+    when "CO2" then 4
+    end
+  end 
+
   def set_plant
     @plant = Plant.find(params[:plant_id])
   end
 
   def measurement_params
-    params.require(:measurement).permit(:measurement_type, :value, :measured_at)
+    params.require(:measurement).permit(
+      :temperature,
+      :humidity,
+      :ec,
+      :ph,
+      :CO2,
+      :measured_at,
+      :comment
+    )
   end
 end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,8 +1,8 @@
 class Measurement < ApplicationRecord
   belongs_to :plant
 
-  enum :measurement_type, [ :temperature, :humidity, :ec, :ph ]
-  def measurement_type_i18n
-    I18n.t("enums.measurement.measurement_type.#{measurement_type}")
-  end
+  # enum :measurement_type, [ :temperature, :humidity, :ec, :ph ]
+  # def measurement_type_i18n
+  #   I18n.t("enums.measurement.measurement_type.#{measurement_type}")
+  # end
 end
